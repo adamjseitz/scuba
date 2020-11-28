813a0acc7eaa2efb7182884bbc0594f616c2c3ef
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-11-27 21:25:16 -0500

    scuba: Remove stub verbose_msg
    These two calls don't add much information for the user. If we want this
    level of detail, we should use the logging API.

--- scuba/scuba.py
+++ scuba/scuba.py
@@ -14,10 +14,6 @@ from .dockerutil import make_vol_opt
 from .utils import shell_quote_cmd, flatten_list, StringIO
 
 
-def verbose_msg(fmt, *args):
-    # TODO: remove
-    pass
-
 class ScubaError(Exception):
     pass
 
@@ -213,11 +209,9 @@ class ScubaDive:
         '''
         if not self.context.script:
             # No user-provided command; we want to run the image's default command
-            verbose_msg('No user command; getting command from image')
             default_cmd = get_image_command(self.context.image)
             if not default_cmd:
                 raise ScubaError('No command given and no image-specified command')
-            verbose_msg('{} Cmd: "{}"'.format(self.context.image, default_cmd))
             self.context.script = [shell_quote_cmd(default_cmd)]
 
         # Make scubainit the real entrypoint, and use the defined entrypoint as
